cdfc9e7668570a461526fd20b95140cc9cb08190
ToblerX <super.yaroslav05@gmail.com>
2025-05-03 18:55:48 +0200

    GET and POST books

--- app/api/book.py
+++ app/api/book.py
@@ -1,7 +1,16 @@
+from typing import List
 from fastapi import APIRouter
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from app import db as app_db
+from app import services, schemas
 
 book_router = APIRouter()
 
-@book_router.get("/books", tags=["Books"])
-async def get_books():
-    return {"books": "No books yet"}
+
+@book_router.post("/books", tags=["Books"], response_model=schemas.BookModel)
+async def create_book(book: schemas.BookCreate, current_session: Session = Depends(app_db.get_db)):
+    return services.create_book(book, current_session)
+@book_router.get("/books", tags=["Books"], response_model=List[schemas.BookModel])
+async def get_books(current_session: Session = Depends(app_db.get_db)):
+    return services.get_books(current_session)
