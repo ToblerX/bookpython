b6fbba712ebeafed944598bb906c5c2e038c18bc
ToblerX <super.yaroslav05@gmail.com>
2025-05-04 20:25:34 +0200

    password hashing

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,11 +1,18 @@
 from sqlalchemy.orm import Session
-from fastapi import Depends
-from .. import schemas, services
+from passlib.context import CryptContext
+from .. import schemas
 from .. import db as app_db
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 def create_user(user: schemas.UserCreate, current_session: Session):
     new_user = app_db.User(**user.dict())
+    new_user.hashed_password = get_password_hash(user.hashed_password)
     current_session.add(new_user)
     current_session.commit()
     current_session.refresh(new_user)
