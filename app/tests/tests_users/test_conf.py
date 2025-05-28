e6357763c1372e44c069f2d7b3cca524b96d8d5e
ToblerX <super.yaroslav05@gmail.com>
2025-05-28 12:51:30 +0200

    more wishlist tests

--- app/tests/tests_users/test_conf.py
+++ app/tests/tests_users/test_conf.py
@@ -29,58 +29,64 @@ mock_users = [
     },
 ]
 
+fake_book_1 = {
+    "book_id": 1,
+    "book_name": "The AI Revolution",
+    "book_author": "Jane Smith",
+    "book_description": "An insightful look into the rise of artificial intelligence.",
+    "book_price": 29.99,
+    "supply": 50,
+    "book_cover_path": "app/static/images/books/cover_not_available.jpg",
+    "genres": [],
+    "created_at": datetime(2025, 5, 1, 10, 30),
+    "updated_at": datetime(2025, 5, 10, 15, 45),
+}
+fake_book_2 = {
+    "book_id": 2,
+    "book_name": "Mysteries of the Orient",
+    "book_author": "John Doe",
+    "book_description": "A thrilling mystery novel set aboard the legendary Orient Express.",
+    "book_price": 19.99,
+    "supply": 30,
+    "book_cover_path": "app/static/images/books/cover_not_available.jpg",
+    "genres": [],
+    "created_at": datetime(2025, 4, 20, 9, 15),
+    "updated_at": datetime(2025, 5, 5, 11, 0),
+}
 # Mock books to return from the endpoint
 mock_books = [
-    {
-        "book_id": 1,
-        "book_name": "The AI Revolution",
-        "book_author": "Jane Smith",
-        "book_description": "An insightful look into the rise of artificial intelligence.",
-        "book_price": 29.99,
-        "supply": 50,
-        "book_cover_path": "app/static/images/books/cover_not_available.jpg",
-        "genres": [],
-        "created_at": datetime(2025, 5, 1, 10, 30),
-        "updated_at": datetime(2025, 5, 10, 15, 45),
-    },
-    {
-        "book_id": 2,
-        "book_name": "Mysteries of the Orient",
-        "book_author": "John Doe",
-        "book_description": "A thrilling mystery novel set aboard the legendary Orient Express.",
-        "book_price": 19.99,
-        "supply": 30,
-        "book_cover_path": "app/static/images/books/cover_not_available.jpg",
-        "genres": [],
-        "created_at": datetime(2025, 4, 20, 9, 15),
-        "updated_at": datetime(2025, 5, 5, 11, 0),
-    },
+    fake_book_1,
+    fake_book_2,
 ]
+
+fake_admin = schemas.UserModel(
+    user_id=0,
+    username="admin",
+    email="admin@gmail.com",
+    disabled=False,
+    verified=True,
+    role="admin",
+    created_at=datetime.now(),
+    updated_at=datetime.now(),
+)
+
+fake_user = schemas.UserModel(
+    user_id=10,
+    username="user",
+    email="user@example.com",
+    disabled=False,
+    verified=True,
+    role="user",
+    created_at=datetime.now(),
+    updated_at=datetime.now(),
+)
 
 
 # Override dependency for admin user
 def override_get_current_active_user_admin():
-    return schemas.UserModel(
-        user_id=0,
-        username="admin",
-        email="admin@gmail.com",
-        disabled=False,
-        verified=True,
-        role="admin",
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-    )
+    return fake_admin
 
 
 # Override dependency for non-admin user
 def override_get_current_active_user_user():
-    return schemas.UserModel(
-        user_id=10,
-        username="user",
-        email="user@example.com",
-        disabled=False,
-        verified=True,
-        role="user",
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-    )
+    return fake_user
--- app/tests/tests_users/test_users_me_wishlist.py
+++ app/tests/tests_users/test_users_me_wishlist.py
@@ -1,7 +1,27 @@
 from unittest.mock import patch
 
-from app import services
-from .test_conf import override_get_current_active_user_user, client, app, mock_books
+import pytest
+from starlette import status
+
+from app import services, errors
+from .test_conf import (
+    override_get_current_active_user_user,
+    client,
+    app,
+    mock_books,
+    fake_user,
+    fake_book_1,
+)
+
+
+@pytest.fixture()
+def add_to_wishlist_response():
+    return {
+        "message": "Book added to wishlist",
+        "status": status.HTTP_200_OK,
+        "username": fake_user.username,
+        "book_name": fake_book_1["book_name"],
+    }
 
 
 def test_get_users_me_wishlist_unauthorized():
@@ -21,5 +41,67 @@ def test_get_users_me_wishlist_authorized():
         assert response.status_code == 200
         assert response.json()[0]["book_name"] == "The AI Revolution"
         assert response.json()[1]["book_name"] == "Mysteries of the Orient"
+
+        app.dependency_overrides.clear()
+
+
+def test_post_users_me_wishlist_unauthorized():
+    response = client.post("/users/me/wishlist")
+    assert response.status_code == 401
+
+
+def test_post_users_me_wishlist_authorized_valid(add_to_wishlist_response):
+    with patch(
+        "app.services.add_to_wishlist",
+        return_value=add_to_wishlist_response,
+    ):
+        app.dependency_overrides[services.get_current_active_user] = (
+            override_get_current_active_user_user
+        )
+        response = client.post(f"/users/me/wishlist?book_id={fake_book_1['book_id']}")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["book_name"] == fake_book_1["book_name"]
+        assert response.json()["username"] == fake_user.username
+        app.dependency_overrides.clear()
+
+
+def test_delete_users_me_wishlist_unauthorized():
+    response = client.delete(f"/users/me/wishlist?book_id=1")
+    assert response.status_code == 401
+
+
+def test_delete_users_me_wishlist_authorized_valid():
+    delete_response = {
+        "message": "Book removed from wishlist.",
+        "status": status.HTTP_200_OK,
+        "username": fake_user.username,
+        "book_name": fake_book_1["book_name"],
+    }
+
+    with patch("app.services.delete_from_wishlist", return_value=delete_response):
+        app.dependency_overrides[services.get_current_active_user] = (
+            override_get_current_active_user_user
+        )
+
+        response = client.delete(f"/users/me/wishlist?book_id={fake_book_1['book_id']}")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["book_name"] == fake_book_1["book_name"]
+        assert response.json()["username"] == fake_user.username
+        assert response.json()["message"] == "Book removed from wishlist."
+
+        app.dependency_overrides.clear()
+
+
+def test_delete_users_me_wishlist_book_not_associated():
+    with patch(
+        "app.services.delete_from_wishlist", return_value=errors.BookNotAssociated
+    ):
+        app.dependency_overrides[services.get_current_active_user] = (
+            override_get_current_active_user_user
+        )
+
+        response = client.delete(f"/users/me/wishlist?book_id=999")
+        assert "The book is not in the wishlist." in response.text
 
         app.dependency_overrides.clear()
